77bc413f5c36a50f6987ba1530b97e8cfa344b98
Vijay <Vijay@Elavarasan>
2024-08-20 18:36:51 +0530

    First commit

---
+++ JavaRoadMap/src/java_week1/Datatype.java
@@ -0,0 +1,13 @@
+package java_week1;
+
+class Datatype {
+	double num = 12.3;
+	float f1 = 123.5f;
+	String name="Vijay";
+	int b=20;
+	boolean mybool=true;
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ JavaRoadMap/src/java_week1/Operators.java
@@ -0,0 +1,23 @@
+package java_week1;
+
+import java.util.Scanner;
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the value a:");
+		int a= input.nextInt();
+		System.out.print("Enter the value b:");
+		int b= input.nextInt();
+		
+		System.out.println("The value of "+ a*b);//Multiplication
+		System.out.print(a > b);
+		System.out.print(a = b);
+	
+		
+		
+	}
+
+}
