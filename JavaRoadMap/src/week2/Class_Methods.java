4203db7a85859269579b8ef6cb08c3f72dccb950
Vijay <Vijay@Elavarasan>
2024-08-22 14:40:14 +0530

    added java

--- JavaRoadMap/src/week2/Class_Methods.java
+++ JavaRoadMap/src/week2/Class_Methods.java
@@ -1,20 +1,20 @@
-package learning_java;
+package week2;
 
-public class Methods {
+public class Class_Methods {
 	
-	static void myMethod() {
+	public static void myMethod() {
 		System.out.println("I just got Selected");
 	}
-	static void myModel(String fname) {
+	public static void myModel(String fname) {
 		System.out.println(fname +" You did not Selected");
 	}
-	static void myMethods() {
+	private static void myMethods() {
 		System.out.println("you just Waiting list");
 	}
-	static int myMeth(int a) {
+	public static int myMeth(int a) {
 		return 5+a;
 	}
-	static int myMeths(int a,int b) {
+	public static int myMeths(int a,int b) {
 		return a*b;
 	}
 
