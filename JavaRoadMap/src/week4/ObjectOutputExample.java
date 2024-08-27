792e9f17fe7d3dd975078689b1ff6e7f3c8333cd
Vijay <Vijay@Elavarasan>
2024-08-27 19:03:29 +0530

    Week4

--- JavaRoadMap/src/week4/ObjectOutputExample.java
+++ JavaRoadMap/src/week4/ObjectOutputExample.java
@@ -20,18 +20,18 @@ public class ObjectOutputExample {
     	Dog dog1 = new Dog("Tyson", "Labrador");
 
         // Serialize the Dog object to a file
-        try (FileOutputStream fileOut = new FileOutputStream("");
+        try (FileOutputStream fileOut = new FileOutputStream("file.out");
              ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
             
             objOut.writeObject(dog1);
-            System.out.println("Serialized data is saved in file.ser");
+            System.out.println("Serialized data is saved in file.out");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // Deserialize the Dog object from the file
-        try (FileInputStream fileIn = new FileInputStream("file.ser");
+        try (FileInputStream fileIn = new FileInputStream("file.out");
              ObjectInputStream objIn = new ObjectInputStream(fileIn)) {
             
             Dog newDog = (Dog) objIn.readObject();
